351cc8dc147d8a59681d2fa22a066751b276b511
Farhad Ali Irinel GUL <farhad_ali.gul@stud.acs.upb.ro>
2023-12-24 01:11:52 +0200

    Working MPI

--- mpi.cpp
+++ mpi.cpp
@@ -28,24 +28,21 @@ void free_all_images(PPMImage **images, int start, int stop,
     free(to_apply);
 }
 
-PPMImage *(**read_all_filters(int *number_of_filters))(PPMImage *) {
+void read_all_filters(int *number_of_filters, int *indeces) {
     scanf("%d", number_of_filters);
-    PPMImage *(**to_apply)(PPMImage *) = (PPMImage * (**)(PPMImage *))
-        malloc(sizeof(PPMImage * (*)()) * (*number_of_filters + 1));
     for (int i = 0; i < *number_of_filters; i++) {
         int filter_id;
         scanf("%d", &filter_id);
-        to_apply[i] = filters[filter_id];
+        indeces[i] = filter_id;
     }
-    return to_apply;
 }
 
 void apply_all_filters(PPMImage **images, int start, int stop,
-                       PPMImage *(**to_apply)(PPMImage *),
+                       int *to_apply,
                        int number_of_filters) {
     for (int i = start; i < stop; i++) {
         for (int j = 0; j < number_of_filters; j++) {
-            images[i] = to_apply[j](images[i]);
+            images[i] = filters[to_apply[j]](images[i]);
         }
     }
 }
@@ -61,7 +58,11 @@ int main(int argc, char *argv[]) {
     if (number_of_images < 0 || number_of_images > 50) {
         printf("Invalid number of images\n");
         return 1;
-    }
+    }    
+    
+    PPMImage **images;
+    images = (PPMImage **)malloc(sizeof(PPMImage *) * number_of_images);
+    read_all_images(images, 0, number_of_images);
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);  // process id
@@ -69,34 +70,48 @@ int main(int argc, char *argv[]) {
 
     int number_of_images_per_process = number_of_images / size;
 
-    PPMImage **images;
-    images = (PPMImage **)malloc(sizeof(PPMImage *) * number_of_images);
-    read_all_images(images, 0, number_of_images);
+
 
     int number_of_filters;
-    PPMImage *(**to_apply)(PPMImage *);
+    int *to_apply;
 
-    if (rank == 0) {
-        
-        to_apply = read_all_filters(&number_of_filters);
-        // Send with MPI_Bcast
-        MPI_Bcast(&number_of_filters, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        MPI_Bcast(to_apply, number_of_filters, MPI_UNSIGNED_LONG, 0,
-                  MPI_COMM_WORLD);
+    int start = rank * number_of_images_per_process;
+    int stop = start + number_of_images_per_process;
+    int hasRest = 0;
 
-        if (number_of_images % size != 0) {
-            number_of_images_per_process = number_of_images % size;
-        }
+    if (number_of_images % size != 0) {
+        hasRest = number_of_images % size;
     }
 
-    printf("Images in current rank %d has %d images\n", rank,
-           number_of_images_per_process);
+    to_apply = (int *)malloc(sizeof(int) * (1 + number_of_filters));
+    memset(to_apply, 0, sizeof(int) * (1 + number_of_filters));
 
-    //apply_all_filters(images, 0, number_of_images_per_process, to_apply,
-    //                  number_of_filters);
+    if (rank == 0) {
+        read_all_filters(&number_of_filters, to_apply);
+    }
+    // Send with MPI_Bcast
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Bcast(&number_of_filters, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(to_apply, number_of_filters + 1, MPI_INT, 0, MPI_COMM_WORLD);
+    
+    // Apply filters
+    apply_all_filters(images, start, stop, to_apply, number_of_filters);
 
-    write_all_images(images, 0, number_of_images_per_process);
-    // free_all_images(images, start, stop, to_apply);
+
+    write_all_images(images, start, stop);
+    if (hasRest != 0) {
+        int current_rank = 0;
+        while (hasRest != 0) {
+            int start = number_of_images - hasRest;
+            int stop = start + 1;
+            if (rank == current_rank) {
+                apply_all_filters(images, start, stop, to_apply, number_of_filters);
+                write_all_images(images, start, stop);
+            }
+            current_rank++;
+            hasRest--;
+        }
+    }
     MPI_Finalize();
     return 0;
 }
